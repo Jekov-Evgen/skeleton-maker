807a11ad8b78b8ce0d46bd15e6d1cc2508251242
Jekov-Evgen <jena1245481te@gmail.com>
2024-11-21 22:38:45 +0200

    create main_window.py

--- Logics/Qt/logic_for_Qt.py
+++ Logics/Qt/logic_for_Qt.py
@@ -1,5 +1,5 @@
 import os
-from Logics.Qt.skeleton_code.code import CONST_COD_MAIN
+from Logics.Qt.skeleton_code.code import CONST_COD_MAIN, CONST_COD_MAIN_WINDOW
 
 def create_project_Qt(name : str):
     desktop_path = os.path.join(os.path.expanduser("~"), r"OneDrive\Desktop")
@@ -8,6 +8,19 @@ def create_project_Qt(name : str):
     
     name_main = "main.py"
     main_file = os.path.join(new_path, name_main)
+    folder_UI = os.path.join(new_path, "GUI")
+    file_main_window = os.path.join(folder_UI, "main_window.py")
+    folder_style = os.path.join(folder_UI, "style")
+    style_file = os.path.join(folder_style, "style_Qt.py")
+    
+    os.mkdir(folder_UI)
+    os.mkdir(folder_style)
     
     with open(main_file, 'w') as f:
         f.write(CONST_COD_MAIN)
+    
+    with open(file_main_window, 'w') as f:
+        f.write(CONST_COD_MAIN_WINDOW)
+    
+    with open(style_file, 'w'):
+        pass
--- Logics/Qt/skeleton_code/code.py
+++ Logics/Qt/skeleton_code/code.py
@@ -9,3 +9,17 @@ if __name__ == "__main__":
     
     app.exec()
 """
+
+CONST_COD_MAIN_WINDOW = """
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QMainWindow, QWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle("Ваш заголовок")
+
+        
+        self.show()
+"""
