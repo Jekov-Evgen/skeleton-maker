619d46a7f51e5b8bc0287202ae711f49ccd4481d
Jekov-Evgen <jena1245481te@gmail.com>
2024-11-21 23:08:41 +0200

    create func django

--- Logics/Django/logic_for_django.py
+++ Logics/Django/logic_for_django.py
@@ -1,4 +1,9 @@
 import os
 
 def create_project_Django(name : str):
-    print(name)
+    desktop_path = os.path.join(os.path.expanduser("~"), r"OneDrive\Desktop")
+    new_path = os.path.join(desktop_path, name)    
+    os.mkdir(new_path)
+    
+    os.chdir(new_path)
+    os.system(f"django-admin startproject {name}")
