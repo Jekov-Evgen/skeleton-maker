2be8d6185fdd831cf49f18c3a346e525fdec8315
Jekov-Evgen <jena1245481te@gmail.com>
2024-11-21 23:18:38 +0200

    create gitignore for django

--- Logics/Django/logic_for_django.py
+++ Logics/Django/logic_for_django.py
@@ -1,4 +1,5 @@
 import os
+from Logics.Django.skeleton_code.cod import CONST_GIT
 
 def create_project_Django(name : str):
     desktop_path = os.path.join(os.path.expanduser("~"), r"OneDrive\Desktop")
@@ -7,3 +8,8 @@ def create_project_Django(name : str):
     
     os.chdir(new_path)
     os.system(f"django-admin startproject {name}")
+    
+    os.chdir(os.path.join(new_path, name))
+    
+    with open(".gitignore", 'w') as f:
+        f.write(CONST_GIT)
